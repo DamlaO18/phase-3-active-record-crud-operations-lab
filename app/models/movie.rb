2bd3b2841ba7d9c3880408b5d7fd5ca43e0ab891
Damla Osman <damlaaosman@gmail.com>
2022-10-18 20:52:35 +1100

    commit tree 18111f2bf92b7bed5390d36a5641126b45ec70db

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,22 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(title)
+        movie = Movie.create(title: title)
+    end
+
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find_by_id(id)
+    end
+
+    def update_with_attributes(attributes)
+        self.update(attributes)
+    end
 end
